2ffb9ca836ea40a28830f72ea5c29d044350cb7f
waz2211 <fawaz.mumtaz24@smk.belajar.id>
2026-09-24 11:19:22 +0700

    first commit feature transaksi

---
+++ app/Filament/Resources/Transactions/Tables/TransactionsTable.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Resources\Transactions\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class TransactionsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('account_id')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('user_id')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('no_slip')
+                    ->searchable(),
+                TextColumn::make('tanggal')
+                    ->date()
+                    ->sortable(),
+                TextColumn::make('jenis_transaksi')
+                    ->searchable(),
+                TextColumn::make('jumlah_transaksi')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
